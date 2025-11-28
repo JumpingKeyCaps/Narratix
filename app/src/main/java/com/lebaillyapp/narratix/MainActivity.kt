923ba5b0325f11f93064bc7b4b8555640494e406
Samuel Lebailly <sam.meusa@gmail.com>
2025-11-28 02:26:21 +0100

    feature(main/): Add the init of the sensor viewmodel + debut screen test.

--- app/src/main/java/com/lebaillyapp/narratix/MainActivity.kt
+++ app/src/main/java/com/lebaillyapp/narratix/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,9 +17,15 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import com.lebaillyapp.narratix.assistant.ui.screen.CinematicDemoScreen
 import com.lebaillyapp.narratix.assistant.ui.screen.DemoScreen1
+import com.lebaillyapp.narratix.gyrosensor.SensorCrosshairDebugOverkill
+import com.lebaillyapp.narratix.gyrosensor.SensorViewModel
 import com.lebaillyapp.narratix.ui.theme.NarratixTheme
+import kotlin.getValue
 
 class MainActivity : ComponentActivity() {
+
+    private val sensorViewModel: SensorViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,6 +35,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             NarratixTheme {
                 //DemoScreen1()
+
+              //  SensorCrosshairDebugOverkill(viewModel = sensorViewModel)
 
                 CinematicDemoScreen()
             }
