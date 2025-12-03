b38999869000baf168a06917c0ccbf03f42acc09
Samuel Lebailly <sam.meusa@gmail.com>
2025-12-03 18:51:44 +0100

    feature(main/): Tweak the main to load the demo screen.

--- app/src/main/java/com/lebaillyapp/narratix/MainActivity.kt
+++ app/src/main/java/com/lebaillyapp/narratix/MainActivity.kt
@@ -5,17 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import com.lebaillyapp.narratix.assistant.decoration.LiquidBackground
+import com.lebaillyapp.narratix.assistant.decoration.UltimateChromeLiquidDemo
 import com.lebaillyapp.narratix.assistant.ui.screen.CinematicDemoScreen
 import com.lebaillyapp.narratix.assistant.ui.screen.DemoScreen1
 import com.lebaillyapp.narratix.assistant.ui.screen.ParallaxScreen
@@ -42,6 +36,11 @@ class MainActivity : ComponentActivity() {
            //     ParallaxScreen(viewModel = sensorViewModel )
 
                 CinematicDemoScreen()
+
+
+            //    UltimateChromeLiquidDemo()
+
+
             }
         }
     }
